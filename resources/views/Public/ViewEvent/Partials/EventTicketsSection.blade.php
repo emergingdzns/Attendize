d1cdaef80c0c507f5e20be8e12480128bede818d
Andrew Christensen <andrew@emergingdesigns.com>
2019-05-03 06:07:56 -0700

    moved gratuity up

--- resources/views/Public/ViewEvent/Partials/EventTicketsSection.blade.php
+++ resources/views/Public/ViewEvent/Partials/EventTicketsSection.blade.php
@@ -62,10 +62,10 @@
                                                 if ($event->organiser_fee_fixed > 0 || $event->organiser_fee_percentage > 0) {
                                                     $pluses[] = lang("Public_ViewEvent.booking_fees");
                                                 }
-                                                if ($event->gratuity_fixed > 0 || $event->gratuity_percentage > 0)) {
+                                                if ($event->gratuity_fixed > 0 || $event->gratuity_percentage > 0) {
                                                     $pluses[] = lang("Public_ViewEvent.gratuity");
                                                 }
-                                                if ($event->charge_tax && $ticket->total_price*($event->organiser->tax_value)/100) > 0) {
+                                                if ($event->charge_tax && ($ticket->total_price*($event->organiser->tax_value)/100) > 0) {
                                                     $pluses[] = $event->organiser->tax_name;
                                                 }
                                             ?>
