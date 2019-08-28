2769d0ea5dfd74b7fdaab03dd182857a160ffc9e
Andrew Christensen <andrew@emergingdesigns.com>
2019-08-28 08:55:10 -0600

    PDF changes

--- resources/views/Public/ViewEvent/Partials/PDFTicket.blade.php
+++ resources/views/Public/ViewEvent/Partials/PDFTicket.blade.php
@@ -69,14 +69,10 @@
                             <div class="event_details">
                                 <h4>@lang("Ticket.event")</h4>
                                 {{$event->title}}
-                                <h4>@lang("Ticket.organiser")</h4>
-                                {{$event->organiser->name}}
-                                <h4>@lang("Ticket.venue")</h4>
-                                {{$event->venue_name}}
                                 <h4>@lang("Ticket.start_date_time")</h4>
                                 {{$event->start_date->format('Y-m-d H:i')}}
-                                <h4>@lang("Ticket.end_date_time")</h4>
-                                {{$event->end_date->format('Y-m-d H:i')}}
+                                <h4>Location</h4>
+                                {{$event->venue_name}}
                             </div>
 
                             <div class="attendee_details">
@@ -84,10 +80,6 @@
                                 {{$attendee->first_name.' '.$attendee->last_name}}
                                 <h4>@lang("Ticket.ticket_type")</h4>
                             	{{$attendee->ticket->title}}
-                                <h4>@lang("Ticket.order_ref")</h4>
-                            	{{$order->order_reference}}
-                                <h4>@lang("Ticket.attendee_ref")</h4>
-                            	{{$attendee->reference}}
                                 <h4>@lang("Ticket.price")</h4>
 								@php
 	                            	// Calculating grand total including tax
@@ -97,8 +89,10 @@
 	                            @endphp
 	                            {{money($grand_total, $order->event->currency)}} (inc. {{money($attendee->ticket->total_booking_fee, $order->event->currency)}} @lang("Public_ViewEvent.inc_fees") (inc. {{money($tax_amt, $order->event->currency)}} {{$event->organiser->tax_name}})
 	                            <br><br>{{$event->organiser->tax_name}} ID: {{ $event->organiser->tax_id }}
+                                <h4>@lang("Ticket.order_ref")</h4>
+                                {{$order->order_reference}}
                             </div>
-			</div>
+			            </div>
                         <div class="barcode">
                             {!! DNS2D::getBarcodeSVG($attendee->private_reference_number, "QRCODE", 6, 6) !!}
                         </div>
